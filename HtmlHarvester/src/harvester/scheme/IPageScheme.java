484f7cd61cc581b49aecb0a4cf5d5658ff0f361f
nyradr <nyradr@protonmail.com>
2016-05-06 16:03:35 +0200

    page scheme functions definition

--- HtmlHarvester/src/harvester/scheme/IPageScheme.java
+++ HtmlHarvester/src/harvester/scheme/IPageScheme.java
@@ -1,5 +1,29 @@
 package harvester.scheme;
 
-public interface IPageScheme {
+import java.util.Set;
 
+/**
+ * Page scheme interface
+ * @author nyradr
+ */
+public interface IPageScheme {
+	
+	/**
+	 * Get the page name
+	 * @return
+	 */
+	public String getPageName();
+	
+	/**
+	 * Get page data names
+	 * @return set of data names
+	 */
+	public Set<String> getDatasName();
+	
+	/**
+	 * Get data scheme from name
+	 * @param name data name
+	 * @return data scheme or null
+	 */
+	public IDataScheme getData(String name);
 }
--- HtmlHarvester/src/harvester/scheme/PageScheme.java
+++ HtmlHarvester/src/harvester/scheme/PageScheme.java
@@ -1,5 +1,35 @@
 package harvester.scheme;
 
-class PageScheme {
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Page scheme implementation
+ * @author nyradr
+ */
+class PageScheme implements IPageScheme{
+	
+	private Map<String, IDataScheme> datas;
+	
+	public PageScheme() throws SchemeParseError{
+	}
+	
+	@Override
+	public String getPageName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getDatasName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDataScheme getData(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
