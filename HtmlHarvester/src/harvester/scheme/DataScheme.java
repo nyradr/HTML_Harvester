243666c3410d8ed80b16bdd280923f6459df4d64
nyradr <nyradr@protonmail.com>
2016-05-06 16:13:11 +0200

    data scheme functions definition

--- HtmlHarvester/src/harvester/scheme/DataScheme.java
+++ HtmlHarvester/src/harvester/scheme/DataScheme.java
@@ -1,5 +1,46 @@
 package harvester.scheme;
 
-class DataScheme {
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Implementation of {@link IDataScheme}
+ * @author nyradr
+ */
+class DataScheme implements IDataScheme{
+	
+	private String name;
+	private boolean all;
+	private DataType type;
+	private String baseXPath;
+	private Set<String> allXPath;
+	private Map<String, Set<String>> args;
+	
+	public DataScheme() throws SchemeParseError{
+	}
+	
+	@Override
+	public String getDataName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DataType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<String> getAllXPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- HtmlHarvester/src/harvester/scheme/IDataScheme.java
+++ HtmlHarvester/src/harvester/scheme/IDataScheme.java
@@ -1,5 +1,35 @@
 package harvester.scheme;
 
-public interface IDataScheme {
+import java.util.Set;
 
+/**
+ * Data scheme
+ * @author nyradr
+ */
+public interface IDataScheme {
+	
+	/**
+	 * Get data name
+	 * @return
+	 */
+	public String getDataName();
+	
+	/**
+	 * True if all result of the XPath is returned<br>
+	 * False if only the first result of the XPath is returned
+	 * @return
+	 */
+	public boolean isAll();
+	
+	/**
+	 * Get data type
+	 * @return
+	 */
+	public DataType getType();
+	
+	/**
+	 * Get all possible XPath for this data
+	 * @return
+	 */
+	public Set<String> getAllXPath();
 }
