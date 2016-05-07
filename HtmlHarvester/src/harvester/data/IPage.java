cd1e7b7dded6ffd1fd35241f883c37f2b7fb8051
nyradr <nyradr@protonmail.com>
2016-05-07 14:14:32 +0200

    add IPage functions declarations

--- HtmlHarvester/src/harvester/data/IPage.java
+++ HtmlHarvester/src/harvester/data/IPage.java
@@ -1,5 +1,44 @@
 package harvester.data;
 
-public interface IPage {
+import java.util.List;
+import java.util.Set;
 
+import harvester.scheme.IPageScheme;
+
+/**
+ * Scanned page
+ * @author nyradr
+ */
+public interface IPage {
+	
+	/**
+	 * Get the page name
+	 * @return
+	 */
+	public String getName();
+	
+	/**
+	 * Get the page scheme
+	 * @return
+	 */
+	public IPageScheme getScheme();
+	
+	/**
+	 * Get the page data names
+	 * @return
+	 */
+	public Set<String> getDatasName();
+	
+	/**
+	 * Get a list of all collected data
+	 * @return
+	 */
+	public List<IData> getAll();
+	
+	/**
+	 * Get data by data name
+	 * @param name
+	 * @return
+	 */
+	public IData getByName(String name);
 }
